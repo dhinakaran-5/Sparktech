e2568488d3eb6047f822681d1fba86bd352a4a3c
unknown <smartdhina19@gmail.com>
2025-09-23 00:57:27 +0530

    vercel3

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import cookieParser from "cookie-parser";
 import express from "express" 
 import cors from 'cors'
 import connectDB from "./configs/db.js";
-const serverless = require("serverless-http");
+
 
 import 'dotenv/config';
 import userRouter from "./routes/userRoute.js";
@@ -52,13 +52,13 @@ app.use("/api/address",addressRouter)
 app.use("/api/order",orderRouter)
 
 
-app.listen(port,()=>{
-    console.log(`SERVER IS RUNNING ON http://localhost:${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`SERVER IS RUNNING ON http://localhost:${port}`)
+// })
 
 
 
 
-
+const serverless = require("serverless-http");
 module.exports = app;
 module.exports.handler = serverless(app);
